5f29322d71c1bf16e347ffd78e51a11b9007f1a1
v-a-l-e-r-i <valeria.dyvak@gmail.com>
2025-12-28 11:53:08 +0200

    Implement inline submission type buttons after start message

---
+++ main.py
@@ -0,0 +1,35 @@
+import asyncio
+import os
+from aiogram import Bot, Dispatcher
+from aiogram.types import Message
+from aiogram.filters import CommandStart
+from dotenv import load_dotenv
+
+from keyboards.submission import submission_type_keyboard
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+
+async def start_handler(message: Message):
+    await message.answer(
+        "Вітаю у Провулку! 🌙\n\n"
+        "Надішли нам свою творчість ✨\n"
+        "(вірш, верлібр, проза, візуальне мистецтво)"
+        "Оберіть, що саме ви хочете нам надіслати:",
+        reply_markup=submission_type_keyboard()
+    )
+
+
+async def main():
+    bot = Bot(token=BOT_TOKEN)
+    dp = Dispatcher()
+
+    dp.message.register(start_handler, CommandStart())
+
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
