64f0c9b8589417083b20a668e44bfc816d05f7d1
v-a-l-e-r-i <valeria.dyvak@gmail.com>
2026-01-12 18:32:09 +0200

    Remove from keywords, keyword for accept music from users

--- keyboards/submission.py
+++ keyboards/submission.py
@@ -7,7 +7,6 @@ def submission_type_keyboard() -> InlineKeyboardMarkup:
             [InlineKeyboardButton(text="✍️ Надіслати поезію", callback_data="send_poetry")],
             [InlineKeyboardButton(text="📖 Надіслати прозу", callback_data="send_prose")],
             [InlineKeyboardButton(text="🎨 Надіслати візуальне мистецтво", callback_data="send_visual")],
-            [InlineKeyboardButton(text="🎵 Надіслати музику", callback_data="send_music")],
             [InlineKeyboardButton(text="✨ Надіслати інше", callback_data="send_other")],
         ]
     )
